260d13794f4e1bea880600067deae8939848dc44
Nikaestu <lehir-dylan@hotmail.com>
2023-04-22 15:31:55 +0200

    #7: Mise en composant du code affichant l'image et le nom sur la liste des personnages afin d'avoir un code plus visible et plus maintenable.

--- RickAndMortyAPI/RickAndMortyAPI/CharactersListView.swift
+++ RickAndMortyAPI/RickAndMortyAPI/CharactersListView.swift
@@ -19,46 +19,7 @@ struct CharactersListView: View {
                 ForEach(data.characters, id: \.id) { character in
                     NavigationLink(
                         destination: CharacterDetailView(character: character),
-                        label: {
-                            HStack {
-                                //Règle métier 4 : En mode "Gratuite", l'application affiche l'initiale du prénom du personnage à la place de son image
-                                if (isPrenium) {
-                                    if let image = character.image,
-                                       let url = URL(string: image) {
-                                        KFImage(url)
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
-                                            .frame(width: 50, height: 50)
-                                            .cornerRadius(25)
-                                    } else {
-                                        RoundedRectangle(cornerRadius: 25)
-                                            .frame(width: 50, height: 50)
-                                            .foregroundColor(.gray)
-                                    }
-                                } else {
-                                    ZStack {
-                                        let letter: String = String(character.name.prefix(1))
-                                        Text(letter)
-                                        Circle()
-                                            .fill(Color.black)
-                                            .frame(width: 50, height: 50)
-                                        Text(letter)
-                                            .font(.title)
-                                            .foregroundColor(.white)
-                                    }
-                                }
-                                //Règle métier 3 : pour mettre rick et morty en avant, les personnages dont l’espèce est ‘Human’ doivent aussi avoir leur prénom en majuscules
-                                if(character.species == "Human") {
-                                    Text(character.name.uppercased())
-                                } else {
-                                    //Règle métier 2 : les personnages doivent avoir leur nom affiché en majuscules
-                                    let firstName = character.name.components(separatedBy: " ").first!.lowercased()
-                                    let lastName = character.name.components(separatedBy: " ").dropFirst().joined(separator: " ").uppercased()
-                                    let customName = firstName + " " + lastName
-                                    Text(customName)	
-                                }
-                            }
-                        }
+                        label: {CharacterRow(character: character)}
                     )
                 }
                 if data.shouldDisplayNextPage {
